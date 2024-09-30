4bed860c16cbb4f545421a72688602e6111e6869
tay <tay@havenmc.org>
2024-09-30 05:05:42 -0400

    Updated and reformated literally everything

--- static/assets/js/lanyard.js
+++ static/assets/js/lanyard.js
@@ -1,19 +1,21 @@
 const dcstat = document.getElementById("dcstat");
 const apiEndpoint = `https://api.lanyard.rest/v1/users/1050531216589332581`;
 fetch(apiEndpoint)
-    .then(response => response.json())
-    .then(data => {
-        const dcStatus = data.data.discord_status;
-        if (dcStatus === "offline") {
-            dcstat.src = "/assets/imgs/offline.png"; /* dcstat.innerHTML = "{OFFLINE}" */
-        } else if (dcStatus === "online") {
-            dcstat.src = "/assets/imgs/online.png"; /* dcstat.innerHTML = "{ONLINE}" */
-        } else if (dcStatus === "idle") {
-            dcstat.src = "/assets/imgs/away.png"; /* dcstat.innerHTML = "{AWAY}" */
-        } else if (dcStatus === "dnd") {
-            dcstat.src = "/assets/imgs/dnd.png"; /* dcstat.innerHTML = "{DND}" */
-        }
-    })
-    .catch(error => {
-        console.error('Error fetching Discord status:', error);
-    });
+  .then((response) => response.json())
+  .then((data) => {
+    const dcStatus = data.data.discord_status;
+    if (dcStatus === "offline") {
+      dcstat.src =
+        "/assets/imgs/offline.png"; /* dcstat.innerHTML = "{OFFLINE}" */
+    } else if (dcStatus === "online") {
+      dcstat.src =
+        "/assets/imgs/online.png"; /* dcstat.innerHTML = "{ONLINE}" */
+    } else if (dcStatus === "idle") {
+      dcstat.src = "/assets/imgs/away.png"; /* dcstat.innerHTML = "{AWAY}" */
+    } else if (dcStatus === "dnd") {
+      dcstat.src = "/assets/imgs/dnd.png"; /* dcstat.innerHTML = "{DND}" */
+    }
+  })
+  .catch((error) => {
+    console.error("Error fetching Discord status:", error);
+  });
