889994906b83b7f2b758a82bc54473d35ff9d913
Valerie <valerie@mysty.one>
2024-11-05 12:51:39 -0500

    Updated about and home pages, etc etc

---
+++ static/assets/js/namewriter.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const names = ["Valerie", "Tee", "Riya", "Rhea", "Kara"];
+  const nameElement = document.getElementById("dynamic-name");
+  let index = 0;
+  let currentName = "";
+  let typingIndex = 0;
+  let isDeleting = false;
+
+  function type() {
+    if (isDeleting) {
+      currentName = names[index].substring(0, typingIndex--);
+    } else {
+      currentName = names[index].substring(0, typingIndex++);
+    }
+
+    nameElement.textContent = currentName;
+
+    if (!isDeleting && typingIndex === names[index].length) {
+      setTimeout(() => {
+        isDeleting = true;
+      }, 1000);
+    } else if (isDeleting && typingIndex === 0) {
+      isDeleting = false;
+      index = (index + 1) % names.length;
+    }
+
+    setTimeout(type, isDeleting ? 100 : 200);
+  }
+
+  type();
+});
