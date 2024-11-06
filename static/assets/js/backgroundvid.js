71be0d1a78fcde790b548f211ca275ebcf4f422a
Valerie <valerie@mysty.one>
2024-11-06 00:48:25 -0500

    Renamed testing to about-testing, Updated about.

--- static/assets/js/backgroundvid.js
+++ static/assets/js/backgroundvid.js
@@ -1,15 +1,15 @@
 const videos = [
-    'https://index.havenmc.org/website/assets/mp4/fightsong.mp4',
-    'https://index.havenmc.org/website/assets/mp4/usurper.mp4',
-    'https://index.havenmc.org/website/assets/mp4/lmwasabi.mp4'
+  "https://index.havenmc.org/website/assets/mp4/fightsong.mp4",
+  "https://index.havenmc.org/website/assets/mp4/usurper.mp4",
+  "https://index.havenmc.org/website/assets/mp4/lmwasabi.mp4",
 ];
 
 function setRandomVideo() {
-    const randomVideo = videos[Math.floor(Math.random() * videos.length)];
-    const videoSource = document.querySelector('#bgVideo source');
-    videoSource.src = randomVideo;
-    const bgVideo = document.getElementById('bgVideo');
-    bgVideo.load();
+  const randomVideo = videos[Math.floor(Math.random() * videos.length)];
+  const videoSource = document.querySelector("#bgVideo source");
+  videoSource.src = randomVideo;
+  const bgVideo = document.getElementById("bgVideo");
+  bgVideo.load();
 }
 
 window.onload = setRandomVideo;
