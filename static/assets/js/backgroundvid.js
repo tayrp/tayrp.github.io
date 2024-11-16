1aa7162232304e6ffb9a9ea34f097b55275ed549
Valerie <valerie@mysty.one>
2024-11-16 11:24:04 -0500

    Updated backgroundvid.js

--- static/assets/js/backgroundvid.js
+++ static/assets/js/backgroundvid.js
@@ -3,6 +3,14 @@ const videos = [
   "https://index.havenmc.org/website/assets/mp4/usurper.mp4",
   "https://index.havenmc.org/website/assets/mp4/lmwasabi.mp4",
   "https://index.havenmc.org/website/assets/mp4/canyoufeelmyheart.mp4",
+  "https://index.havenmc.org/website/assets/mp4/itgirl.mp4",
+  "https://index.havenmc.org/website/assets/mp4/BeautifulMistakes.webm",
+  "https://index.havenmc.org/website/assets/mp4/studioapartment.webm",
+  "https://index.havenmc.org/website/assets/mp4/californiasky.mp4",
+  "https://index.havenmc.org/website/assets/mp4/bekind.webm",
+  "https://index.havenmc.org/website/assets/mp4/letmedownslowly.webm",
+  "https://index.havenmc.org/website/assets/mp4/ifwehaveeachother.webm",
+  "https://index.havenmc.org/website/assets/mp4/why.webm",
 ];
 
 function setRandomVideo() {
