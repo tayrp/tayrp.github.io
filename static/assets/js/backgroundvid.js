4938e8527a14e352050c1bbdc8478b97de74ec1c
Valerie <valerie@rose-haven.org>
2025-04-10 16:29:31 -0400

    Update

--- static/assets/js/backgroundvid.js
+++ static/assets/js/backgroundvid.js
@@ -1,16 +1,5 @@
 const videos = [
-  "https://index.havenmc.org/website/assets/mp4/fightsong.mp4",
-  "https://index.havenmc.org/website/assets/mp4/usurper.mp4",
-  "https://index.havenmc.org/website/assets/mp4/lmwasabi.mp4",
-  "https://index.havenmc.org/website/assets/mp4/canyoufeelmyheart.mp4",
-  "https://index.havenmc.org/website/assets/mp4/itgirl.mp4",
-  "https://index.havenmc.org/website/assets/mp4/BeautifulMistakes.webm",
-  "https://index.havenmc.org/website/assets/mp4/studioapartment.webm",
-  "https://index.havenmc.org/website/assets/mp4/californiasky.mp4",
-  "https://index.havenmc.org/website/assets/mp4/bekind.webm",
-  "https://index.havenmc.org/website/assets/mp4/letmedownslowly.webm",
-  "https://index.havenmc.org/website/assets/mp4/ifwehaveeachother.webm",
-  "https://index.havenmc.org/website/assets/mp4/why.webm",
+  "https://im.valerie.lol/r/NKLFb9.webm?compress=false"
 ];
 
 function setRandomVideo() {
